4959d468225308c1d256b07246d9b7a326dca7a7
Lucas Safeno <lugnorio@gmail.com>
2022-12-30 13:27:15 -0300

    PHP - MVC - View,Twig,Composer

--- rafaelCapoani/mvc/app/Controller/HomeController.php
+++ rafaelCapoani/mvc/app/Controller/HomeController.php
@@ -4,9 +4,19 @@ class HomeController{
 	public function index(){
 
 		try{
-					//echo 'Home';
+
 			$colecPostagens = Postagem::selecionaTodos();
-			var_dump($colecPostagens);
+
+			$loader = new \Twig\Loader\FilesystemLoader('app/View');
+			$twig = new \Twig\Environment($loader);
+			$template = $twig->load('home.html');
+
+			$parametros = array();
+			$parametros['postagens'] = $colecPostagens;
+
+			$conteudo =  $template->render($parametros);
+			echo $conteudo;
+		
 		}catch(Exception $e){
 			echo $e->getMessage();
 		}
--- rafaelCapoani/mvc/index.php
+++ rafaelCapoani/mvc/index.php
@@ -8,6 +8,8 @@ require_once('app/Controller/ErroController.php');
 
 require_once('app/Model/Postagem.php');
 
+require_once('vendor/autoload.php');
+
 $template = file_get_contents("app/Template/estrutura.html");
 
 
