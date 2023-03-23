6d1309d9bb50dd52e50c705546dd09d51ebfe434
Lucas Safeno <lugnorio@gmail.com>
2023-03-23 17:21:19 -0300

    Loja Virtual com PHP#16 - Parametro dos métodos parte 2 - ERROR

--- wdev/projeto-phpoo/Public/bootstrap/bootstrap.php
+++ wdev/projeto-phpoo/Public/bootstrap/bootstrap.php
@@ -1,18 +1,20 @@
 <?php
 use App\Classes\Template;
+use App\Classes\Parameters;
+
 
 $template = new Template();
 $twig = $template->init();
 
 
 $callController = new App\Controllers\Controller;
-$calledControler = $callController->controller();
-
-$controller = new $calledControler();
+$calledController = $callController->controller();
+$controller = new $calledController();
 $controller->setTwig($twig);
 
 $callMethod = new App\Controllers\Method;
 $method = $callMethod->method($controller);
 
-
-$controller->$method();
+$parameters = new Parameters;
+$parameter = $paramters->getParamterMethod($controller, $method);
+$controller->$method($parameter);
