4a9b81be8c2f94c997220fdbe1416d6bdf3059d8
Lucas Safeno <lugnorio@gmail.com>
2023-03-23 13:00:55 -0300

    Loja Virtual com PHP #12 - Criando Classe de template

---
+++ wdev/projeto-phpoo/App/Classes/Template.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Classes;
+
+class Template{
+
+    public function loader(){
+        return new \Twig_Loader_Filesystem(['../App/Views/Site', '../App/Views/Admin']);
+    }// loader
+
+    public function init(){
+        $twig = new \Twig_Environment($this->loader(), [
+            'debug' => true,
+            //'cache' => '',
+            'auto_reload' => true
+        ]);
+
+        return $twig;
+    } // init
+
+} // Template
