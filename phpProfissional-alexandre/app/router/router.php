f22b5ad6adcbe9fd03cca5c9b1d94cca29b0468b
Lucas Safeno <lugnorio@gmail.com>
2023-06-08 13:00:37 -0300

    PHP Profissional Alexadnre Cardoso - #03 FIX

--- phpProfissional-alexandre/app/router/router.php
+++ phpProfissional-alexandre/app/router/router.php
@@ -4,28 +4,35 @@ function routes(){
 } // routes
 function existsMatchUriInArrayRoutes($uri, $routes){
     if(array_key_exists($uri, $routes)){
-        return [];
+        return [$uri => $routes[$uri]];
     }
 
     return [];
 } //  existsMatchUriInArrayRoutes
 
-
+function regularExpressioinMatcharrayRoutes($uri, $routes){
+    return array_filter(
+        $routes,
+        function($value) use ($uri){
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/')); 
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
 function router(){
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
     $routes = routes();
 
-    //$mathedUri = existsMatchUriInArrayRoutes($uri, $routes);
+    $mathedUri = existsMatchUriInArrayRoutes($uri, $routes);
  
-    array_filter(
-        $routes,
-        function($value) use ($uri){
-            $regex = str_replace('/', '\/', ltrim($value, '/'));
-            return preg_match("/^$regex/", ltrim($uri)); 
-        },
-        ARRAY_FILTER_USE_KEY
-    );
+    if(empty($mathedUri)){
+        $mathedUri = regularExpressioinMatcharrayRoutes($uri, $routes);
+    }
+
+    var_dump($uri);
+  
 
 } //router
 
