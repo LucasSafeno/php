a638628805c87401e4ac2b6355a45b1edf3ba310
Lucas Safeno <lugnorio@gmail.com>
2023-07-10 11:10:30 -0300

    PHP B7WEB - 10 - Estrutura MVC - Views

--- phpProfissionalB7web/06-php-SuperAvancado/03-mvc/views/template.php
+++ phpProfissionalB7web/06-php-SuperAvancado/03-mvc/views/template.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo BASE_URL?>assets/css/style.css">
+    <script src="<?php echo BASE_URL; ?>assets/js/js.js"></script>
     <title>Meu Site</title>
 </head>
 <body>
