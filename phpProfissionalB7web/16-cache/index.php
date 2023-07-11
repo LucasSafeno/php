e08c0a7746ae0645cf19463effa00f6fcce16bd1
Lucas Safeno <lugnorio@gmail.com>
2023-07-10 21:51:39 -0300

    PHP B7WEB - 17 Cache básico

--- phpProfissionalB7web/16-cache/index.php
+++ phpProfissionalB7web/16-cache/index.php
@@ -1,4 +1,36 @@
 <?php 
+class Cache{
+
+    private $cache;
+
+    public function setVar($nome, $valor){
+        $this->readCache();
+        $this->cache->$nome = $valor;
+        $this->saveCache();
+    } //setVar
+
+    public function getVar($nome){
+        $this->readCache();
+
+        return $this->cache->$nome;
+    } //getVar
+
+    private function readCache(){
+        $this->cache = new stdClass();
+        if(file_exists('cache.cache')){
+            $this->cache = json_decode(file_get_contents('cache.cache'));
+        }
+    } //readCache
+
+    private function saveCache(){
+        file_put_contents('cache.cache', json_encode($this->cache));
+    }// saveCache
 
 
+
+} // cache
+
+
+$cache = new Cache();
+$cache->setVar("nome", "Lucas");
 ?>
