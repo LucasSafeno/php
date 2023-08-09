e6b2462622c05b12a602ada8ee5d5bb9f2facf38
Lucas Safeno <lugnorio@gmail.com>
2023-08-09 13:44:55 -0300

    Curso Desenvolvimento web - MVC - 512 Views

--- mvc_udemy/App/Controllers/indexController.php
+++ mvc_udemy/App/Controllers/indexController.php
@@ -3,13 +3,31 @@ namespace App\Controllers;
 
 class IndexController{
 
+    private $view;
+
+    public function __construct(){
+        $this->view = new \stdClass();
+    } // construct
+
     public function index(){
-        echo "index";
+        $this->view->dados = array("Sofá", "Cadeira","Cama");
+        $this->render('index');
     } // index
 
     public function sobreNos(){
-        echo "Sobre nós";
+        $this->view->dados = array("Notebook", "Computador Desktop");
+        $this->render('sobre_nos');
     } // sobreNos
+
+    public function render($view){
+        $classeAtual = get_class($this);
+
+        $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+        $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+
+
+        require_once "../App/Views/".$classeAtual."/".$view.".phtml";
+    } // view
 
 
 }// IndexController
--- mvc_udemy/App/Route.php
+++ mvc_udemy/App/Route.php
@@ -1,26 +1,13 @@
 <?php 
 namespace App;
 
-class Route{
+use MF\Init\Bootstrap;
 
-    private $routes;
+class Route extends Bootstrap{
 
-
-    public function __construct(){
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }// construct
-
-    public function getRoutes(){
-       return $this->routes;
-    } // getRoutes
-
-    public function setRoutes(array $routes){
-        $this->routes = $routes;
-    } // setRoutes
 
     // Rotas que a app possui
-    public function initRoutes(){
+    protected function initRoutes(){
         $routes['home'] = array(
             'route'         => '/',
             'controller'    => 'indexController',
@@ -37,26 +24,6 @@ class Route{
     } // initRoutes()
 
 
-    public function run($url){
-        
-        foreach($this->getRoutes() as $key => $route){
-
-            if($url == $route['route']){
-                $class = "App\\Controllers\\".ucfirst($route['controller']);
-
-                $controller = new $class;
-
-                $action = $route['action'];
-                $controller->$action();
-            }
-        }
-    } // run
-
-    //Verifica qual rota usuário está acessando
-    public function getUrl(){
-            //parse_url retorna URL em um array dividindo seus parametros passados na URl
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    } //getUrl
 
 } // Route
 
