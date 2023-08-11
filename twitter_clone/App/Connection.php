d7103f73de7a0fbd290b1d99d1cf5d4a6c294980
Lucas Safeno <lugnorio@gmail.com>
2023-08-11 15:00:01 -0300

    Desenvolvimento web completo - Twitter Clone - 519 Registrando usuários

--- twitter_clone/App/Connection.php
+++ twitter_clone/App/Connection.php
@@ -7,7 +7,7 @@ class Connection{
 
     public static function getDb(){
         try{
-            $conn = new \PDO("mysql:host=localhost;dbname=mvc;charset=utf8", "root", "" );
+            $conn = new \PDO("mysql:host=localhost;dbname=twitter_clone;charset=utf8", "root", "" );
 
             return $conn;
         }catch(PDOException $e){
---
+++ twitter_clone/App/Models/Usuario.php
@@ -0,0 +1,61 @@
+<?php 
+namespace App\Models;
+
+use MF\Model\Model  ;
+
+class Usuario extends Model{
+
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+
+    public function __get($atributo){
+        return $this->$atributo;
+    } // __get()
+
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
+    } // __set()
+
+    public function salvar(){
+        $query = "INSERT INTO twitter_clone.usuarios(nome, email, senha) VALUE (:nome, :email, :senha)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(":nome", $this->__get('nome'));
+        $stmt->bindValue(":email", $this->__get('email'));
+        $stmt->bindValue(":senha", $this->__get('senha'));
+        $stmt->execute();
+
+        return $this;
+    } // salvar
+
+    public function validarCadastro(){
+        $valido = true;
+
+        if(strlen($this->__get('nome')) <= 3){
+            $valido = false;
+        }
+        
+        if(strlen($this->__get('email')) <= 3){
+            $valido = false;
+        }
+
+        
+        if(strlen($this->__get('senha')) <= 3){
+            $valido = false;
+        }
+
+        return $valido;
+    }// validarCadastro
+
+    public function getUsuarioPorEmail(){
+        $query = "SELECT email FROM twitter_clone.usuarios WHERE email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(":email",$this->__get('email'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    } //getUsuarioPorEmail
+
+}// Usuario
+?>
