eb5301bb32592149a74263dc496dc1deec981af3
Lucas Safeno <lugnorio@gmail.com>
2023-08-15 14:22:25 -0300

    Desenvolvimento Web completo - Udemy - twitter clone - 522 Criandoa  timeline, incluindo e listando tweets

--- twitter_clone/App/Controllers/AppController.php
+++ twitter_clone/App/Controllers/AppController.php
@@ -8,15 +8,45 @@ use MF\Model\Container;
 class AppController extends Action{
 
     public function timeline(){
-       session_start();
+        session_start();
+        $this->validaAutenticao();
+        
+            // recuperar tweet
+            $tweet = Container::getModel('Tweet');
+            $tweet->__set('id_usuario', $_SESSION['id']);
+            $tweets = $tweet->getAll();
 
-        if(!empty($_SESSION['id']) && !empty($_SESSION['nome'])){
+            $this->view->tweets = $tweets;
+
            $this->render('timeline');
-        }else{
-            header("Location: /?login=erro");
-        }
+       
       
     } // timeline
+
+
+    public function tweet(){
+
+    session_start();
+    $this->validaAutenticao();
+           
+            $tweet = Container::getModel('Tweet');
+            
+            $tweet->__set('tweet', $_POST['tweet']);
+            $tweet->__set('id_usuario', $_SESSION['id']);
+
+            $tweet->salvar();
+            header("Location: /timeline");
+
+
+    } //tweet
+
+    public function validaAutenticao(){
+        session_start();
+        if(!isset($_SESSION['id']) || empty($_SESSION['nome'])){
+            header("Location: /?login=erro");
+            
+        }
+    } //validaAutenticao
 
 }// AppController
 ?>
