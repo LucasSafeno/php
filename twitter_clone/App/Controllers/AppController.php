c3b7873332eb27d37fc6f901848ed5fa8e85dde1
Lucas Safeno <lugnorio@gmail.com>
2023-08-15 23:54:36 -0300

    Desenvolvimento web completo - Udemy - Twitter Clone - 523 Pesquisando por outros usuários

--- twitter_clone/App/Controllers/AppController.php
+++ twitter_clone/App/Controllers/AppController.php
@@ -48,5 +48,25 @@ class AppController extends Action{
         }
     } //validaAutenticao
 
+    public function quemSeguir(){
+        $this->validaAutenticao();
+
+        
+
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+        $usuario = array();
+
+        if($pesquisarPor != ''){
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('nome', $pesquisarPor);
+            $usuarios = $usuario->getAll();
+        }
+
+        $this->view->usuarios = $usuarios;
+        $this->render('quemSeguir');
+
+    } // quemSeguir
+
 }// AppController
 ?>
