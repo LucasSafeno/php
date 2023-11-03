847e9a68ca9cef0616ed73ddb222677d5d96845a
Lucas Safeno <lugnorio@gmail.com>
2023-11-03 10:09:20 -0300

    Class response FIX #22:00

--- wdev/index.php
+++ wdev/index.php
@@ -4,7 +4,11 @@ require "vendor/autoload.php";
 use App\Controller\Pages\Home;
 
 $objRequest = new \App\Http\Request;
+$objResponse = new \App\Http\Response(200, 'Olá mundo');
 
+
+$objResponse->sendResponse();
+exit;
 echo Home::getHome();
 
 ?>
