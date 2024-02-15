cb12bbffacfa89690a46abf7363c7e38fdae8fef
Lucas Safeno <lugnorio@gmail.com>
2024-02-15 01:30:22 -0300

    Hugo Vasconcelhos - Sistema SAS - #F0262 Aula 41 - Array com diversos dados

--- hugo_vasconcelhos/saas/autenticar.php
+++ hugo_vasconcelhos/saas/autenticar.php
@@ -10,17 +10,38 @@ $query = $pdo->prepare("SELECT * FROM usuarios WHERE (email = :usu OR cpf = :usu
 $query->bindValue(":usu", $usuario);
 $query->bindValue(":senha", $senha_crip);
 $query->execute();
-$res = $query->fetch(PDO::FETCH_ASSOC);
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
 $total_reg = count($res);
 
 if($total_reg > 0){
-	$_SESSION['id'] = $res['id'];
-	$_SESSION['nome'] = $res['nome'];
+	$_SESSION['id'] = $res[0]['id'];
+	$_SESSION['nome'] = $res[0]['nome'];
+	$_SESSION['nivel'] = $res[0]['nivel'];
+	$_SESSION['empresa'] = $res[0]['empresa'];
 
-	echo $_SESSION['nome'];
+	// armazenar no storage o id
+	$id = $res[0]['id'];
+	$nivel_usu = $res[0]['nivel'];
+	echo "<script>localStorage.setItem('id_usu', '$id')</script>";
+	echo "<script>localStorage.setItem('nivel_usu', '$nivel_usu')</script>";
+
+	if($res[0]['ativo'] != 'Sim'){
+		echo '<script>alert("Usuário inativo. Contate o administrador");</script>';
+		echo '<script>window.location="index.php"</script>';
+		exit();
+	}
+
+	if($_SESSION['nivel'] == 'SAS'){
+		echo '<script>window.location="sas"</script>';	
+	}else{
+		echo '<script>window.location="sistema"</script>';	
+	}
+
+
 }else{
 	echo '<script>alert("Dados incorretos");</script>';
 	echo '<script>window.location="index.php"</script>';
+	exit();
 }
 
 
---
+++ hugo_vasconcelhos/saas/logout.php
@@ -0,0 +1,8 @@
+<?php 
+/*session_start();
+session_destroy(); */
+
+echo "<script>localStorage.setItem('id_usu','')</script>";
+echo "<script>localStorage.setItem('nivel_usu','')</script>";
+echo '<script>window.location="index.php"</script>';
+ ?>
