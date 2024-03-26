358137546a7a26370f40dd8e31a35e60a2d79206
Lucas Safeno <lugnorio@gmail.com>
2024-03-26 15:37:25 -0300

    Docker - #F015 04 - Criando arquivo docker-composeyml

---
+++ formacao_fullstack_upinside/00-docker/00-volume/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+<title> Docker Volume - Aula Update</title>
+</head>
+<body>
+<h1>Lucas Tenório</h1>
+  <?php 
+    $nome = "Teste";
+    var_dump($nome);
+  ?>
+</body>
+</html>
